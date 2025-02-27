07c19e1704d153c6506ad4bd4286daffbb895cc9
Redoy Islam <mdredoyislam84@gmail.com>
2025-02-27 12:04:35 +0600

    DV Themes

---
+++ assets/js/main.js
@@ -0,0 +1,175 @@
+(function($) {
+    'use strict';
+    // sticky menu
+    var header = $('.menu-sticky');
+    var win = $(window);
+    win.on('scroll', function() {
+       var scroll = win.scrollTop();
+       if (scroll < 1) {
+           header.removeClass("sticky");
+       } else {
+           header.addClass("sticky");
+       }
+
+        $("section").each(function() {
+        var elementTop = $(this).offset().top - $('#rs-header').outerHeight();
+            if(scroll >= elementTop) {
+                $(this).addClass('loaded');
+            }
+        });
+    });
+    // onepage nav
+    var navclose = $('#onepage-menu');
+    if(navclose.length){
+       $(".nav-menu li").on("click", function () {
+           if ($(".showhide").is(":visible")) {
+               $(".showhide").trigger("click");
+           }
+       });
+       
+       if ($.fn.onePageNav) {
+           $(".nav-menu").onePageNav({
+               currentClass: "active-menu"
+           });
+       }
+    }
+    // collapse hidden  
+    var navMain = $(".navbar-collapse");
+    navMain.on("click", "a:not([data-toggle])", null, function () {
+        navMain.collapse('hide');
+    });
+    //preloader
+    $(window).on( 'load', function() {
+      $("#braintech-load").delay(1000).fadeOut(500);
+    })
+    // Parallax Stuff
+    if ($(".stuff").length) {
+        var stuff = $('.stuff').get(0);
+        var parallaxInstance = new Parallax(stuff);
+    }
+    if ($(".stuff2").length) {
+        var stuff = $('.stuff2').get(0);
+        var parallaxInstance = new Parallax(stuff);
+    }
+    if ($(".stuff3").length) {
+        var stuff = $('.stuff3').get(0);
+        var parallaxInstance = new Parallax(stuff);
+    }
+    if ($(".stuff4").length) {
+        var stuff = $('.stuff4').get(0);
+        var parallaxInstance = new Parallax(stuff);
+    }
+    $("a.clickup").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 500, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+    // OwlCarousel
+    $('.rs-carousel').each(function() {
+        var owlCarousel = $(this),
+        loop = owlCarousel.data('loop'),
+        items = owlCarousel.data('items'),
+        dotsEach = owlCarousel.data('doteach'),
+        margin = owlCarousel.data('margin'),
+        stagePadding = owlCarousel.data('stage-padding'),
+        autoplay = owlCarousel.data('autoplay'),
+        autoplayTimeout = owlCarousel.data('autoplay-timeout'),
+        smartSpeed = owlCarousel.data('smart-speed'),
+        dots = owlCarousel.data('dots'),
+        nav = owlCarousel.data('nav'),
+        navSpeed = owlCarousel.data('nav-speed'),
+        xsDevice = owlCarousel.data('mobile-device'),
+        xsDeviceNav = owlCarousel.data('mobile-device-nav'),
+        xsDeviceDots = owlCarousel.data('mobile-device-dots'),
+        smDevice = owlCarousel.data('ipad-device'),
+        smDeviceNav = owlCarousel.data('ipad-device-nav'),
+        smDeviceDots = owlCarousel.data('ipad-device-dots'),
+        smDevice2 = owlCarousel.data('ipad-device2'),
+        smDeviceNav2 = owlCarousel.data('ipad-device-nav2'),
+        smDeviceDots2 = owlCarousel.data('ipad-device-dots2'),
+        mdDevice = owlCarousel.data('md-device'),
+        lgDevice = owlCarousel.data('lg-device'),
+        centerMode = owlCarousel.data('center-mode'),
+        HoverPause = owlCarousel.data('hoverpause'),
+        mdDeviceNav = owlCarousel.data('md-device-nav'),
+        mdDeviceDots = owlCarousel.data('md-device-dots');
+        owlCarousel.owlCarousel({
+            loop: (loop ? true : false),
+            dotsEach: (dotsEach ? true : false),
+            items: (items ? items : 4),
+            lazyLoad: true,
+            center: (centerMode ? true : false),
+            autoplayHoverPause: (HoverPause ? true : false),
+            margin: (margin ? margin : 0),
+            //stagePadding: (stagePadding ? stagePadding : 0),
+            autoplay: (autoplay ? true : false),
+            autoplayTimeout: (autoplayTimeout ? autoplayTimeout : 1000),
+            smartSpeed: (smartSpeed ? smartSpeed : 250),
+            dots: (dots ? true : false),
+            nav: (nav ? true : false),
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+            navSpeed: (navSpeed ? true : false),
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: (xsDevice ? xsDevice : 1),
+                    nav: (xsDeviceNav ? true : false),
+                    dots: (xsDeviceDots ? true : false),
+                    center: false,
+                },
+                576: {
+                    items: (smDevice2 ? smDevice2 : 2),
+                    nav: (smDeviceNav2 ? true : false),
+                    dots: (smDeviceDots2 ? true : false),
+                    center: false,
+                },
+                768: {
+                    items: (smDevice ? smDevice : 3),
+                    nav: (smDeviceNav ? true : false),
+                    dots: (smDeviceDots ? true : false),
+                    center: false,
+                },
+                992: {
+                    items: (mdDevice ? mdDevice : 4),
+                    nav: (mdDeviceNav ? true : false),
+                    dots: (mdDeviceDots ? true : false),
+                },
+                1200: {
+                    items: (lgDevice ? lgDevice : 4),
+                }
+            }
+        });
+    });
+    // Counter Up
+    if ($('.counter-number').length) {  
+        $('.counter-number').counterUp({
+            delay: 20,
+            time: 1500
+        });
+    }
+    // wow init
+    new WOW().init();
+    // scrollTop init
+    var win=$(window);
+    var totop = $('#scrollUp');    
+    win.on('scroll', function() {
+        if (win.scrollTop() > 150) {
+            totop.fadeIn();
+        } else {
+            totop.fadeOut();
+        }
+    });
+    totop.on('click', function() {
+        $("html,body").animate({
+            scrollTop: 0
+        }, 500)
+    });
+})(jQuery);
+
+ 
